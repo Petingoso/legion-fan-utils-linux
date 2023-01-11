a9e6d5b1b90a56a57e5e82ab05522995012a0d4d
Petingoso <petingavasco@protonmail.com>
2023-01-11 18:46:11 +0000

    removed gui stuff

--- main.py
+++
@@ -1,8 +0,0 @@
-from PyQt.QtWidgets import QApplication, QWidgets
-
-
-application = QApplication([sys.argv])
-
-mainWindow = QWidget()
-mainWindow.setGeometry(0, 0, 350, 400)
-mainWindow.setWindowTitle('Hello World')
