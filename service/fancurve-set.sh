4a1a658ca99ff2bfff38669a8f22c6707a110bf8
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-04-19 08:01:19 +0100

    full rewrite solve nvidia bugs and simple syntax * fix typo

--- service/fancurve-set.sh
+++ service/fancurve-set.sh
@@ -4,77 +4,45 @@
 
 POWER_PROFILE=$(cat /sys/firmware/acpi/platform_profile)
 AC_ADAPTER=$(cat /sys/class/power_supply/ADP0/online)
-FOLDER=/etc/lenovo-fan-control/profiles
-KERNEL=$(uname -r)
 #verfiy Nvidia card is not using vfio
 NVIDIA_LOADED=$(lsmod | grep -w "nvidia")
+FOLDER=/etc/lenovo-fan-control/profiles # Location of the profiles
+#GPU_TDP #value of the gpu tdp in watts
+# Fancurve file name (also you can edit FOLDER to edit the location of profiles)
 
 #Pls edit this file for enable GPU TDP
 source /etc/lenovo-fan-control/.env
 
 if  [ $AC_ADAPTER == 1 ]; then
-
     if [ $POWER_PROFILE == quiet ]; then
-        echo "Applying Quiet Mode Profile ﴛ  -> charger..."
-        #Before fan curve to avoid failling to apply
-        #TDP in quiet
-        if [[ $TEAM_GREEN -eq 1 && $NVIDIA_LOADED ]]; then
-            exec nvidia-smi -pl 80 # set to 80W
-        elif [[ $TEAM_RED -eq 1 ]]; then
-            exec rocm-smi --setpoweroverdrive 80 # set to 80W
-        fi
+        GPU_TDP=80 #set to GPU 80W
+        FANCURVE_FILE=$FOLDER/legion-profile-charger-quiet #set the fancurve file
 
-        exec python /usr/local/bin/lenovo-legion-fan-service.py -i $FOLDER/legion-profile-charger-quiet
     elif [ $POWER_PROFILE == balanced ]; then
-        echo "Applying Balance Mode Profile   -> charger..."
+        GPU_TDP=125 #set to GPU 125W
+        FANCURVE_FILE=$FOLDER/legion-profile-charger-balance #set the fancurve file
 
-        #Before fan curve to avoid failling to apply
-        #TDP in balanced
-        if [[ $TEAM_GREEN -eq 1 && $NVIDIA_LOADED ]]; then
-            exec nvidia-smi -pl 125 # set to 125W
-        elif [[ $TEAM_RED -eq 1 ]]; then
-            exec rocm-smi --setpoweroverdrive 125 # set to 125W
-        fi
-
-        exec python /usr/local/bin/lenovo-legion-fan-service.py -i $FOLDER/legion-profile-charger-balance
-    else
-        echo "Applying Performance Mode Profile 龍  -> charger..."
-
-        #Before fan curve to avoid failling to apply
-        #TDP in performance
-        if [[ $TEAM_GREEN -eq 1 && $NVIDIA_LOADED ]]; then
-            exec nvidia-smi -pl 140 # set to 140W
-        elif [[ $TEAM_RED -eq 1 ]]; then
-            exec rocm-smi --setpoweroverdrive 140 # set to 140W
-        fi
-    
-        exec python /usr/local/bin/lenovo-legion-fan-service.py -i $FOLDER/legion-profile-charger-performance
+    elif [ $POWER_PROFILE == performance ]; then
+        GPU_TDP=140 #set to GPU 140W
+        FANCURVE_FILE=$FOLDER/legion-profile-charger-performance #set the fancurve file
     fi
-
 else
     if [ $POWER_PROFILE == quiet ]; then
-        echo "Applying Quiet Mode Profile ﴛ  -> battery..."
+        GPU_TDP=55 #set to GPU 55W
+        FANCURVE_FILE=$FOLDER/legion-profile-battery-quiet #set the fancurve file
 
-        #Before fan curve to avoid failling to apply
-        #TDP in quiet
-        if [[ $TEAM_GREEN -eq 1 && $NVIDIA_LOADED ]]; then
-            exec nvidia-smi -pl 55 # set to 55W
-        elif [[ $TEAM_RED -eq 1 ]]; then
-            exec rocm-smi --setpoweroverdrive 55 # set to 55W
-        fi
-
-        exec python /usr/local/bin/lenovo-legion-fan-service.py -i $FOLDER/legion-profile-battery-quiet
-    else
-        echo "Applying Balance Mode Profile   -> battery..." 
-        
-        #Before fan curve to avoid failling to apply
-        #TDP in quiet
-        if [[ $TEAM_GREEN -eq 1 && $NVIDIA_LOADED ]]; then
-            exec nvidia-smi -pl 65 # set to 65W
-        elif [[ $TEAM_RED -eq 1 ]]; then
-            exec rocm-smi --setpoweroverdrive 65 # set to 65W
-        fi
-
-        exec python /usr/local/bin/lenovo-legion-fan-service.py -i $FOLDER/legion-profile-battery-balance
+    elif [ $POWER_PROFILE == balanced ]; then
+        GPU_TDP=65 #set to GPU 65W
+        FANCURVE_FILE=$FOLDER/legion-profile-battery-balance #set the fancurve file
     fi
+fi
+
+if [[ $TEAM_GREEN -eq 1 && $NVIDIA_LOADED ]]; then
+    ls $FANCURVE_FILE
+    python /usr/local/bin/lenovo-legion-fan-service.py -i $FANCURVE_FILE 
+    nvidia-smi -pl $GPU_TDP
+elif [[ $TEAM_RED -eq 1 ]]; then
+    python /usr/local/bin/lenovo-legion-fan-service.py -i $FANCURVE_FILE
+else
+    python /usr/local/bin/lenovo-legion-fan-service.py -i $FANCURVE_FILE
 fi
